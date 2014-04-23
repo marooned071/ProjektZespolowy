9cf0f84c8323d96aca40a6bb77678c885f9f1d6b
Piotr Byliniak <pb1618@gmail.com>
2014-04-23 15:11:46 +0200

    javascrpit: usprawnienie dzialania formularza tworzacego nowe pytanie

--- django/pz/qrpolls/static/qrpolls/scripts.js
+++ django/pz/qrpolls/static/qrpolls/scripts.js
@@ -1,47 +1,20 @@
-function sayHello() {
-	// alert("Hello World!");
-	//document.writeln("<h1>Hello World!</h1><p>Have a nice day!</p>");
-
-	var node=document.createElement("LI");
-	var textnode=document.createTextNode("Water");
-	node.appendChild(textnode);
-	document.getElementById("myList").appendChild(node);
-}
-
-function addForm1() {
-
-	var button = document.createElement("button")
-	button.setAttribute("onClick","addAnswer1()");
-	var t=document.createTextNode("+ odpowiedz");
-	button.appendChild(t); 
 
 
-	var input = document.createElement("input");
-	input.setAttribute("id", "question"); 
-	input.setAttribute("type", "text"); 
-	input.setAttribute("name", "question"); 
-
-	document.getElementById("newForm1").appendChild(input);
-	document.getElementById("buttons").appendChild(button);
-	
-}
-
-var choiceCounter = 0;
+var choiceCounter = 0; //licznik odpowiedzi, zwieksza sie o jeden za kazdym razem gdy zostaje dodana odpowiedz do formularza 
+// sluzy do inkrementowania id (id kazdej odpowiedzi musi byc inne aby bezproblemowa przeslac je POSTem)
 
 
-function createNewForm(){
+function createForm(){
+
+	//obiekt formularza
 	var form = document.createElement("form");
 	form.setAttribute("method", "POST"); 
 	form.setAttribute("action", "newQuestion");
-	form.setAttribute("id", "newForm");
+	form.setAttribute("id", "form");
 
-	//var CSRF_TOKEN = document.getElementById('hidden-csrf');
 
+	//token ktory jest potrzebny aby Django przyjal dane POST
 	var CSRF_TOKEN = document.getElementById('csrf_token').value;
-
-	//var CSRF_TOKEN = "{{ csrf_token }}";
-
-
 	var token = document.createElement("input");
 	token.setAttribute("type", "hidden"); 
 	token.setAttribute("name", "csrfmiddlewaretoken");
@@ -49,57 +22,99 @@ function createNewForm(){
 	form.appendChild(token);
 
 
+	// pytanie
     var questionText = document.createElement("input"); 
     questionText.setAttribute("id", "question"); 
 	questionText.setAttribute("type", "text"); 
 	questionText.setAttribute("name", "question"); 
 
-	form.appendChild(questionText);
+	// wiersz tabeli ktory tworzy w pierwszej kolumnie jest opis pola, np "Tresc Pytania", a w drugiej odpowiedni komponent
+	var tr = document.createElement("tr");
+	var tdLabel = document.createElement("td");
+	var tdInput = document.createElement("td");
+
+	tr.appendChild(tdLabel);
+	tr.appendChild(tdInput);
+	
+	tdInput.appendChild(questionText);
+	tdLabel.appendChild(document.createTextNode("Tresc pytania: "));
+
+	form.appendChild(tr);
+
+	//przycisk dodajacy miejsce na odpowiedz
+	var buttonAddChoice = document.createElement("button");
+	buttonAddChoice.setAttribute("onClick","addChoice()");
+	var t=document.createTextNode("+ odpowiedz");
+	buttonAddChoice.appendChild(t); 
+	document.getElementById("buttons").appendChild(buttonAddChoice);
+
+
+	//tabela zawierajaca miejsca na pisanie tresci odpowiedzi
+	var table = document.createElement("table");
+	table.setAttribute("id", "choiceTable");
+
+	form.appendChild(table);
+
+	//miejsce na buton wysylajacy formularz do bazy.  osoby div aby button zawsze byl w tym samym miejscu
+	var buttonAddQuestionDiv = document.createElement("div");
+	buttonAddQuestionDiv.setAttribute("id", "buttonAddQuestionDiv");
+	form.appendChild(buttonAddQuestionDiv);
+
+
+	var buttonAddQuestion = document.createElement("button");
+	buttonAddQuestion.setAttribute("onClick","submitForm()");
+	var t2=document.createTextNode("Dodaj Pytanie");
+	buttonAddQuestion.appendChild(t2); 
+	buttonAddQuestionDiv.appendChild(buttonAddQuestion);
+
 
 	document.body.appendChild(form);
-
-
-	var buttonAdd = document.createElement("button");
-	buttonAdd.setAttribute("onClick","addChoice()");
-	var t=document.createTextNode("+ odpowiedz");
-	buttonAdd.appendChild(t); 
-	document.getElementById("buttons").appendChild(buttonAdd);
-
 	choiceCounter = 0;
+
+	//usuwamy przycisk tworzenia formularza, jesli stworzymy nowe pytanie robi sie syf z odpowiedziami trzeba to jakos ogarnac
+	var buttonCreateForm = document.getElementById("buttonCreateForm");
+	document.body.removeChild(buttonCreateForm);
+
 
 }
 
-
+//funckcja dodajaca odpowiedz
 function addChoice(){
 	
 	var newId = "id".concat(choiceCounter); //id0, id1 itp
 	var newName = "choice".concat(choiceCounter); 
 
 	var choice = document.createElement("input");
+
+
+	var tr = document.createElement("tr");
+	var tdLabel = document.createElement("td");
+	var tdInput = document.createElement("td");
+
+	tr.appendChild(tdLabel);
+	tr.appendChild(tdInput);
+	
+	tdInput.appendChild(choice);
+	tdLabel.appendChild(document.createTextNode("odp ".concat(choiceCounter)));
+
+
+
 	choice.setAttribute("id", newId); 
 	choice.setAttribute("type", "text"); 
 	choice.setAttribute("name", newName); 
 
-	document.getElementById("newForm").appendChild(choice);
+	//document.getElementById("form").appendChild(choice);
+	document.getElementById("choiceTable").appendChild(tr);
+
+
+
+
 	choiceCounter = choiceCounter +1;
 }
 
 
-function submitNewForm(){
-	var form = document.getElementById("newForm");
+function submitForm(){
+	var form = document.getElementById("form");
 	form.submit();
-}
-
-function addAnswer1(){
-
-	var x=document.getElementsByName("choice");
-
-	var idConcated = "id".concat(x.length);
-	alert(idConcated);
-	var choice = document.createElement("input");
-	choice.setAttribute("id", idConcated); 
-	choice.setAttribute("type", "text"); 
-	choice.setAttribute("name", "choice"); 
-	document.getElementById("newForm1").appendChild(choice);
 }
 

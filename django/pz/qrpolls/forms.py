1811d7b78d68b6c26606550e2052e6fb5028f054
Piotr Byliniak <pb1618@gmail.com>
2014-06-04 16:27:19 +0200

    TimePicker

--- django/pz/qrpolls/forms.py
+++ django/pz/qrpolls/forms.py
@@ -3,7 +3,7 @@ from django.contrib.admin import widgets
 
 
 class NewMeetingForm(forms.Form):
-    start_date = forms.CharField(max_length=200)
+    start_date = forms.CharField(max_length=200,widget=forms.TextInput(attrs={'autocomplete':'off'}))
     room = forms.CharField(max_length=200)
     subject = forms.CharField(max_length=200)
 
--- django/pz/qrpolls/static/qrpolls/charts-scripts.js
+++ django/pz/qrpolls/static/qrpolls/charts-scripts.js
@@ -12,14 +12,34 @@ function drawChart2(divID,question,choices) {
 		}[b];
 	})
 
-	var data = google.visualization.arrayToDataTable($.parseJSON(choices));
+	
+
+
+	var c =$.parseJSON(choices);
+	
+	var data = google.visualization.arrayToDataTable(c);
 	var t="";
 	t= t.concat(question); 
-	var options = {
-		title: t,
-		pieHole: 0.4,
-		is3D : true
-	};
+
+	var options={};
+
+	if(c[c.length-1][0]=='No Votes'){
+		options = {
+			title: t,
+			pieHole: 0.4,
+			is3D : true,
+			colors: ['#d3d3d3']
+
+		};
+	}
+	else{
+		options = {
+			title: t,
+			pieHole: 0.4,
+			is3D : true
+		};
+	}
+
 
 	var chart = new google.visualization.PieChart(document.getElementById(divID));
 	chart.draw(data, options);
